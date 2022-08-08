662a7097db60e3996cb3be670b0370a7967744af
michel005 <mdgrigoli@hotmail.com.br>
2022-08-07 23:27:13 -0300

    Initial files

---
+++ src/main/java/com/michel/gerfinweb/GerfinwebApplication.java
@@ -0,0 +1,13 @@
+package com.michel.gerfinweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerfinwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerfinwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michel/gerfinweb/GerfinwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michel.gerfinweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerfinwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
